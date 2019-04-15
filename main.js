69702760cbb4d74e9c0da6802b2ac484cadb4d87
Maks <wolfsangel009@gmail.com>
2019-04-16 00:55:41 +0300

    First home work on Udemy

---
+++ main.js
@@ -0,0 +1,17 @@
+let money = +prompt('Введите ваш бюджет на месяц', '');
+let timeData = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+  money,
+  timeData,
+  expenses : {
+    'обязательный расход в этом месяце' : '700$'
+    },
+  optionalExpenses : {},
+  income : [],
+};
+
+const month = 30;
+let result = money/month;
+
+alert(result);
